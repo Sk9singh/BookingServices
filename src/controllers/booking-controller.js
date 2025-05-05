c3d600077ea82be3d9f666ba17246f2896fc70de
Saksham Singh <sakshamsingh8278@gmail.com>
2025-05-05 13:07:49 +0530

    Few changes in booking

--- src/controllers/booking-controller.js
+++ src/controllers/booking-controller.js
@@ -15,8 +15,16 @@ class BookingController {
 
    async sendMessageToQueue(req,res) {
         const channel = await createChannel();
-        const data = {message: 'Success'};
-        publishMessage(channel,REMINDER_BINDING_KEY,JSON.stringify(data));
+        const payload = {
+            data: {
+                subject: 'This is a notification from queue',
+                content: 'Some queue will subscribe this',
+                recepientEmail: 'sksingh8278@gmail.com ',
+                notificationTime: '2024-08-20T09:49:00'
+            },
+            service: 'CREATE_TICKET'
+        };
+        publishMessage(channel,REMINDER_BINDING_KEY,JSON.stringify(payload));
         return res.status(200).json({
             message: 'Successfully published the event'
         });
